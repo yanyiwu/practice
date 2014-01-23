88d76657738352db3ad4305fe43c8acb2eae168b
wyy <wuyanyi09@gmail.com>
2014-01-22 23:00:59 -0800

    ci

--- nodejs/t3/hello.cpp
+++ nodejs/t3/hello.cpp
@@ -1,5 +1,6 @@
 #include <node.h>
 #include <v8.h>
+#include <string>
 
 using namespace v8;
 
@@ -8,9 +9,17 @@ Handle<Value> Method(const Arguments& args) {
   return scope.Close(String::New("world"));
 }
 
+Handle<Value> cut(const Arguments& agrs) {
+    HandleScope scope;
+    std::string s("cut");
+    return scope.Close(String::New(s.c_str()));
+}
+
 void init(Handle<Object> exports) {
   exports->Set(String::NewSymbol("hello"),
       FunctionTemplate::New(Method)->GetFunction());
+  exports->Set(String::NewSymbol("cut"),
+      FunctionTemplate::New(cut)->GetFunction());
 }
 
 NODE_MODULE(hello, init)
--- nodejs/t3/hello.js
+++ nodejs/t3/hello.js
@@ -1,2 +1,3 @@
 var addon = require("./build/Release/hello");
 console.log(addon.hello());
+console.log(addon.cut());
