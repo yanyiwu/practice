eacd0208a8cd6ab4edd44e903c62f13fbfe81637
wyy <wuyanyi09@gmail.com>
2014-04-01 21:28:21 +0800

    ci gtest

--- cpp/gtest/TCase1.cpp
+++
@@ -1,9 +0,0 @@
-#include "gtest/gtest.h"
-#include "Limonp/logger.hpp"
-
-using namespace Limonp;
-
-TEST(Test, Case1)
-{
-    cout << __FILE__ << __LINE__ << endl;
-}
---
+++ cpp/gtest/TFoo.cpp
@@ -0,0 +1,7 @@
+#include "gtest/gtest.h"
+
+TEST(FooTest, Case1)
+{
+    ASSERT_EQ(1, 1);
+    ASSERT_NE(1, 2);
+}
