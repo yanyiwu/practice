bc6e6060f6d21dd76635c07135a7051c714b3475
wyy <wuyanyi09@gmail.com>
2014-11-25 16:43:27 +0800

    ci

---
+++ cpp/libevent2/server3.cpp
@@ -0,0 +1,42 @@
+/* For sockaddr_in */
+#include <netinet/in.h>
+/* For socket functions */
+#include <sys/socket.h>
+/* For fcntl */
+#include <fcntl.h>
+
+#include <event2/event.h>
+#include <event2/buffer.h>
+#include <event2/bufferevent.h>
+
+#include <assert.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+
+struct event *ev;
+
+static void cb(int sock, short which, void *arg) {
+        /* Whoops: Calling event_active on the same event unconditionally
+           from within its callback means that no other events might not get
+           run! */
+
+    printf("%s %d\n", __FILE__ , __LINE__);
+    //event_active(ev, EV_WRITE, 0);
+}
+
+int main(int argc, char **argv) {
+        struct event_base *base = event_base_new();
+
+        ev = event_new(base, -1, EV_PERSIST | EV_READ, cb, NULL);
+
+        event_add(ev, NULL);
+
+        event_active(ev, EV_WRITE, 0);
+
+        event_base_loop(base, 0);
+
+        return 0;
+}
