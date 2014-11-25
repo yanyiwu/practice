bd3ac71a3d6c8f4822a2ade7ccb475b716ffdf38
wyy <wuyanyi09@gmail.com>
2014-11-25 17:13:51 +0800

    ci

--- cpp/libevent2/server3.cpp
+++ cpp/libevent2/server3.cpp
@@ -16,27 +16,49 @@
 #include <stdio.h>
 #include <errno.h>
 
-struct event *ev;
+    struct event *ev;
+    struct event* ev2;
+struct event_base *base;
 
-static void cb(int sock, short which, void *arg) {
-        /* Whoops: Calling event_active on the same event unconditionally
-           from within its callback means that no other events might not get
-           run! */
-
-    printf("%s %d\n", __FILE__ , __LINE__);
-    //event_active(ev, EV_WRITE, 0);
+static void cb2(int sock, short which, void* arg) {
+    printf("cb2 %s %d\n", __FILE__ , __LINE__);
 }
 
+static void cb(int sock, short which, void *arg) {
+    /* Whoops: Calling event_active on the same event unconditionally
+       from within its callback means that no other events might not get
+       run! */
+
+    switch(which) {
+        case EV_WRITE:
+            printf("%s %d\n", __FILE__ , __LINE__);
+            break;
+        case EV_READ:
+            printf("%s %d\n", __FILE__ , __LINE__);
+            break;
+        default:
+            printf("%s %d\n", __FILE__ , __LINE__);
+    }
+    printf("%s %d\n", __FILE__ , __LINE__);
+        //event_active(ev, EV_WRITE, 0);
+    event_active(ev2, 0, 0);
+}
+
+
+struct Arg {
+    int i1;
+    int i2;
+};
+
 int main(int argc, char **argv) {
-        struct event_base *base = event_base_new();
-
-        ev = event_new(base, -1, EV_PERSIST | EV_READ, cb, NULL);
-
-        event_add(ev, NULL);
-
-        event_active(ev, EV_WRITE, 0);
-
-        event_base_loop(base, 0);
-
-        return 0;
+    base = event_base_new();
+    //ev = event_new(base, -1, EV_PERSIST|EV_READ, cb, NULL);
+    ev = event_new(base, -1, EV_READ, cb, NULL);
+    ev2= event_new(base, -1, EV_READ, cb2, NULL);
+    event_add(ev, NULL);
+    event_add(ev2, NULL);
+    //event_active(ev, EV_WRITE, 0);
+    event_active(ev, 0, 0);
+    event_base_dispatch(base);
+    return 0;
 }
