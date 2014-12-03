18ad368e8afa8b9d062d3c002d439c4aaeed3948
wyy <wuyanyi09@gmail.com>
2014-12-03 19:39:50 +0800

    ci

--- thrift/nonblock/gen-cpp/tests_server.skeleton.cpp
+++ thrift/nonblock/gen-cpp/tests_server.skeleton.cpp
@@ -4,13 +4,16 @@
 #include "tests.h"
 #include <thrift/protocol/TBinaryProtocol.h>
 #include <thrift/server/TSimpleServer.h>
+#include <thrift/server/TNonblockingServer.h>
 #include <thrift/transport/TServerSocket.h>
 #include <thrift/transport/TBufferTransports.h>
+#include <thrift/concurrency/PosixThreadFactory.h>
 
 using namespace ::apache::thrift;
 using namespace ::apache::thrift::protocol;
 using namespace ::apache::thrift::transport;
 using namespace ::apache::thrift::server;
+using namespace ::apache::thrift::concurrency;
 
 using boost::shared_ptr;
 
@@ -32,12 +35,16 @@ class testsHandler : virtual public testsIf {
 int main(int argc, char **argv) {
   int port = 9090;
   shared_ptr<testsHandler> handler(new testsHandler());
+
   shared_ptr<TProcessor> processor(new testsProcessor(handler));
-  shared_ptr<TServerTransport> serverTransport(new TServerSocket(port));
-  shared_ptr<TTransportFactory> transportFactory(new TBufferedTransportFactory());
   shared_ptr<TProtocolFactory> protocolFactory(new TBinaryProtocolFactory());
 
-  TSimpleServer server(processor, serverTransport, transportFactory, protocolFactory);
+  // using thread pool with maximum 15 threads to handle incoming requests
+  shared_ptr<ThreadManager> threadManager = ThreadManager::newSimpleThreadManager(15);
+  shared_ptr<PosixThreadFactory> threadFactory = shared_ptr<PosixThreadFactory>(new PosixThreadFactory());
+  threadManager->threadFactory(threadFactory);
+  threadManager->start();
+  TNonblockingServer server(processor, protocolFactory, 8888, threadManager);
   server.serve();
   return 0;
 }
