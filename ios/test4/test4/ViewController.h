442df22cfa4ca50382d527e0b87054c04f795df2
yanyiwu <i@yanyiwu.com>
2014-12-23 20:11:47 +0800

    ci

---
+++ ios/test4/test4/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  test4
+//
+//  Created by yanyiwu on 14/12/23.
+//  Copyright (c) 2014年 yanyiwu. All rights reserved.
+//
+
+/*
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
+*/
---
+++ ios/test4/test4/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  test4
+//
+//  Created by yanyiwu on 14/12/23.
+//  Copyright (c) 2014年 yanyiwu. All rights reserved.
+//
+
+/*
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
+
+*/
