b28632ebfe8b78a88e1ff6a88ea2dd32a1db597d
yanyiwu <i@yanyiwu.com>
2014-12-25 16:31:36 +0800

    add test3.cc

--- unix/lock/test1.c
+++ unix/lock/test1.c
@@ -12,44 +12,43 @@ http://zhu8337797.blog.163.com/blog/static/1706175492011229112420419/
 
 const char *test_file = "/tmp/test_lock";
 
-int main()
-{
-    int file_desc;
-    int byte_count;
-    char *byte_to_write = "A";
-    struct flock region_1;
-    struct flock region_2;
-    int res;
+int main() {
+  int file_desc;
+  int byte_count;
+  char *byte_to_write = "A";
+  struct flock region_1;
+  struct flock region_2;
+  int res;
 
-    file_desc = open(test_file, O_RDWR | O_CREAT, 0666);
-    if (!file_desc) {
-        fprintf(stderr, "Unable to open %s for read/write\n", test_file);
-        exit(EXIT_FAILURE);
-    }
-    /* Put some data in the file*/
-    for( byte_count = 0; byte_count < 100; byte_count++) {
-        (void)write(file_desc, byte_to_write, 1);
-    }
-    /* Set up region 1 with a shared lock, from bytes 10 to 30: */
-    region_1.l_type = F_RDLCK;
-    region_1.l_whence = SEEK_SET;
-    region_1.l_start = 10;
-    region_1.l_len = 20;
-    /* Set up region 2 with an exclusive lock, from bytes 40 to 50: */
-    region_2.l_type = F_WRLCK;
-    region_2.l_whence = SEEK_SET;
-    region_2.l_start = 40;
-    region_2.l_len = 10;
-    /* Now lock the file */
-    printf("Process %d locking file\n", getpid());
-    res = fcntl(file_desc, F_SETLK, &region_1);
-    if (res == -1) fprintf(stderr, "Failed to lock region 1\n");
-    res = fcntl(file_desc, F_SETLK, &region_2);
-    if (res == -1) fprintf(stderr, "Failed to lock region 2\n");
-    /* Wait for a while */
-    sleep(60);
+  file_desc = open(test_file, O_RDWR | O_CREAT, 0666);
+  if (!file_desc) {
+    fprintf(stderr, "Unable to open %s for read/write\n", test_file);
+    exit(EXIT_FAILURE);
+  }
+  /* Put some data in the file*/
+  //for( byte_count = 0; byte_count < 100; byte_count++) {
+  //    (void)write(file_desc, byte_to_write, 1);
+  //}
+  /* Set up region 1 with a shared lock, from bytes 10 to 30: */
+  region_1.l_type = F_RDLCK;
+  region_1.l_whence = SEEK_SET;
+  region_1.l_start = 10;
+  region_1.l_len = 20;
+  /* Set up region 2 with an exclusive lock, from bytes 40 to 50: */
+  region_2.l_type = F_WRLCK;
+  region_2.l_whence = SEEK_SET;
+  region_2.l_start = 40;
+  region_2.l_len = 10;
+  /* Now lock the file */
+  printf("Process %d locking file\n", getpid());
+  res = fcntl(file_desc, F_SETLK, &region_1);
+  if (res == -1) fprintf(stderr, "Failed to lock region 1\n");
+  res = fcntl(file_desc, F_SETLK, &region_2);
+  if (res == -1) fprintf(stderr, "Failed to lock region 2\n");
+  /* Wait for a while */
+  sleep(60);
 
-    printf("Process %d  closing file\n", getpid());
-    close(file_desc);
-    exit(EXIT_SUCCESS);
+  printf("Process %d  closing file\n", getpid());
+  close(file_desc);
+  exit(EXIT_SUCCESS);
 }
--- unix/lock/test2.c
+++ unix/lock/test2.c
@@ -15,75 +15,72 @@ l_pid 一开始被设置为 -1，这是非法值。如果被测试的区域被�
 #include <fcntl.h>
 
 const char *test_file = "/tmp/test_lock";
-#define SIZE_TO_TRY 5
+#define SIZE_TO_TRY 10
 
 void show_lock_info(struct flock *to_show);
 
-int main()
-{
-    int file_desc;
-    int res;
-    struct flock region_to_test;
-    int start_byte;
+int main() {
+  int file_desc;
+  int res;
+  struct flock region_to_test;
+  int start_byte;
 
-    /* Open a file descriptor */
-    file_desc = open(test_file, O_RDWR | O_CREAT, 0666);
-    if (!file_desc) {
-        fprintf(stderr, "Unable to open %s for read/write", test_file);
-        exit(EXIT_FAILURE);
+  /* Open a file descriptor */
+  file_desc = open(test_file, O_RDWR | O_CREAT, 0666);
+  if (!file_desc) {
+    fprintf(stderr, "Unable to open %s for read/write", test_file);
+    exit(EXIT_FAILURE);
+  }
+
+  for (start_byte = 0; start_byte < 60; start_byte += SIZE_TO_TRY) {
+    region_to_test.l_type = F_WRLCK;
+    region_to_test.l_whence = SEEK_SET;
+    region_to_test.l_start = start_byte;
+    region_to_test.l_len = SIZE_TO_TRY;
+    region_to_test.l_pid = -1;
+
+    printf("Testing F_WRLCK on region from %d to %d\n",
+           start_byte, start_byte + SIZE_TO_TRY);
+    /* Now test lock on the file */
+    res = fcntl(file_desc, F_GETLK, &region_to_test);
+    if (res == -1) {
+      fprintf(stderr, "F_GETLK failed\n");
+      exit(EXIT_FAILURE);
     }
-
-    for (start_byte = 0; start_byte < 99; start_byte += SIZE_TO_TRY) {
-        region_to_test.l_type = F_WRLCK;
-        region_to_test.l_whence = SEEK_SET;
-        region_to_test.l_start = start_byte;
-        region_to_test.l_len = SIZE_TO_TRY;
-        region_to_test.l_pid = -1;
-
-        printf("Testing F_WRLCK on region from %d to %d\n", 
-                    start_byte, start_byte + SIZE_TO_TRY); 
-        /* Now test lock on the file */
-        res = fcntl(file_desc, F_GETLK, &region_to_test);
-        if (res == -1) {
-            fprintf(stderr, "F_GETLK failed\n");
-            exit(EXIT_FAILURE);
-        }
-        if (region_to_test.l_pid != -1) {
-            printf("Lock would fail. F_GETLK returned: \n");
-            show_lock_info(&region_to_test);
-        }
-        else {
-            printf("F_WRLCK - Lock would succeed\n");
-        }
-        /* Now repeat the test with a shared(read)lock. Set up the region you wish to test again */
-        region_to_test.l_type = F_RDLCK;
-        region_to_test.l_whence = SEEK_SET;
-        region_to_test.l_start = start_byte;
-        region_to_test.l_len = SIZE_TO_TRY;
-        region_to_test.l_pid = -1;
-        printf("Testing F_RDLCK on region from %d to %d\n", start_byte, start_byte + SIZE_TO_TRY);
-        /* Test the lock on the file again */
-        res = fcntl(file_desc, F_GETLK, &region_to_test);
-        if (res == -1) {
-            fprintf(stderr, "F_GETLK failed\n");
-            exit(EXIT_FAILURE);
-        }
-        if (region_to_test.l_pid != -1) {
-            printf("Lock would fail. F_GETLK return: \n");
-            show_lock_info(&region_to_test);
-        }
-        else {
-            printf("F_RDLCK - Lock would secceed\n");
-        }
+    if (region_to_test.l_pid != -1) {
+      printf("Lock would fail. F_GETLK returned: ");
+      show_lock_info(&region_to_test);
+    } else {
+      printf("F_WRLCK - Lock would succeed\n");
     }
-    close(file_desc);
-    exit(EXIT_SUCCESS);
+    ///* Now repeat the test with a shared(read)lock. Set up the region you wish to test again */
+    //region_to_test.l_type = F_RDLCK;
+    //region_to_test.l_whence = SEEK_SET;
+    //region_to_test.l_start = start_byte;
+    //region_to_test.l_len = SIZE_TO_TRY;
+    //region_to_test.l_pid = -1;
+    //printf("Testing F_RDLCK on region from %d to %d\n", start_byte, start_byte + SIZE_TO_TRY);
+    ///* Test the lock on the file again */
+    //res = fcntl(file_desc, F_GETLK, &region_to_test);
+    //if (res == -1) {
+    //    fprintf(stderr, "F_GETLK failed\n");
+    //    exit(EXIT_FAILURE);
+    //}
+    //if (region_to_test.l_pid != -1) {
+    //    printf("Lock would fail. F_GETLK return: ");
+    //    show_lock_info(&region_to_test);
+    //}
+    //else {
+    //    printf("F_RDLCK - Lock would secceed\n");
+    //}
+  }
+  close(file_desc);
+  exit(EXIT_SUCCESS);
 }
-void show_lock_info(struct flock *to_show)
-{
-    printf("\tl_type %d, ", to_show->l_type);
-    printf("l_whence %d, ", to_show->l_whence);
-    printf("l_start %d, ", (int)to_show->l_start);
-    printf("l_len %d, ", (int)to_show->l_len);
-    printf("l_pid %d\n", to_show->l_pid);
+void show_lock_info(struct flock *to_show) {
+  printf("\tl_type %d, ", to_show->l_type);
+  printf("l_whence %d, ", to_show->l_whence);
+  printf("l_start %d, ", (int)to_show->l_start);
+  printf("l_len %d, ", (int)to_show->l_len);
+  printf("l_pid %d\n", to_show->l_pid);
 }
