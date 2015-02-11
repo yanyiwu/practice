c2dd540e488b48bc3f488534f9170e0d2f54b90c
yanyiwu <i@yanyiwu.com>
2015-02-11 17:25:40 +0800

    rename aszxqw -> yanyiwu

--- go/simplelog_test/test.go
+++ go/simplelog_test/test.go
@@ -1,19 +1,19 @@
 package main
 
 import (
-    log "github.com/aszxqw/simplelog"
+	log "github.com/yanyiwu/simplelog"
 )
 
 func main() {
-    log.Debug("hello simplelog.");
-    log.Info("hello simplelog.");
-    log.Warn("hello simplelog.");
-    log.Error("hello simplelog.");
-    log.Fatal("hello simplelog.");
-    log.SetLevel(log.LEVEL_FATAL)
-    log.Debug("hello simplelog.");
-    log.Info("hello simplelog.");
-    log.Warn("hello simplelog.");
-    log.Error("hello simplelog.");
-    log.Fatal("hello simplelog.");
+	log.Debug("hello simplelog.")
+	log.Info("hello simplelog.")
+	log.Warn("hello simplelog.")
+	log.Error("hello simplelog.")
+	log.Fatal("hello simplelog.")
+	log.SetLevel(log.LEVEL_FATAL)
+	log.Debug("hello simplelog.")
+	log.Info("hello simplelog.")
+	log.Warn("hello simplelog.")
+	log.Error("hello simplelog.")
+	log.Fatal("hello simplelog.")
 }
