e265366a6fd5615d53e23a1859dadff21aa7f4a1
yanyiwu <wuyanyi09@foxmail.com>
2015-04-08 16:31:47 +0800

    update wait

--- sh/wait/run.sh
+++ sh/wait/run.sh
@@ -1,21 +1,36 @@
 ./cmd1.sh &
 p1=$!
-#echo $p1
+pids[1]=$p1
+#echo ${pids[1]}
+
 ./cmd2.sh &
 p2=$!
-#echo $p2
+pids[2]=$p2
+#echo ${pids[2]}
 
-wait $p1
-if [ "x$?" != "x0" ]; then
-    echo "$p1 failed"
-    exit 1
-fi
-
-wait $p2
-if [ "x$?" != "x0" ]; then
-    echo "$p2 failed"
-    exit 1
-fi
+for i in `seq ${#pids[*]}`
+do
+    wait ${pids[$i]}
+    if [ "x$?" != "x0" ]
+    then
+        echo "${pids[$i]} failed."
+        exit 1
+    fi
+done
 
 echo "ok!"
 
+#wait $p1
+#if [ "x$?" != "x0" ]; then
+#    echo "$p1 failed"
+#    exit 1
+#fi
+#
+#wait $p2
+#if [ "x$?" != "x0" ]; then
+#    echo "$p2 failed"
+#    exit 1
+#fi
+#
+#echo "ok!"
+#
