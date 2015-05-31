8d211642d7a231026be96eabab82b6ec2b1c6d12
yanyiwu <wuyanyi09@foxmail.com>
2015-06-01 00:45:38 +0800

    test1

--- nodejs/mongoose/test1/test1.js
+++ nodejs/mongoose/test1/test1.js
@@ -3,16 +3,19 @@ var Schema = mongoose.Schema;
 mongoose.connect("mongodb://localhost:27017/test");
 
 var UserSchema = new Schema({
-    username : { type: String, default: '' ,trim : true},
+    username : { type: String, default: '' ,trim : true, index: {unique: true} },
     password : { type: String, default: '' ,trim : true},
     email: { type: String, default: '' }
-},{collection: 'user'});
+}, {collection: 'user'});
 
 var User = mongoose.model('User', UserSchema);
 
 var u= new User({username:"test",password:"123456",email:"123@qq.com"});
+
 u.save(function(err){
-    console.log(err);
+    if(err) {
+        console.log(err);
+    }
 });
 
 //User.remove({ username: 'test' }, function (err) {
@@ -20,13 +23,17 @@ u.save(function(err){
 //});
 
 User.update({ username: 'test' }, { email: '456@qq.com' }, { multi: true }, function (err, numberAffected, raw) {
-    console.log(err);
-    console.log(numberAffected);
-    console.log(raw);
+    if(err) {
+        console.log(err);
+    }
 });
 
 
 User.find({ username: 'test' }, function(err, docs) {
-    console.log(docs);
+    if(err) {
+        console.log(err);
+    } else {
+        console.log(docs.length);
+    }
 });
 
