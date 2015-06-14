9356b544e8a60a1ea2fe77949ad25d0841c80483
yanyiwu <wuyanyi09@foxmail.com>
2015-06-14 10:40:18 +0800

    jiebago performance

--- nodejs/nodejieba/performance/test.js
+++ nodejs/nodejieba/performance/test.js
@@ -5,12 +5,14 @@ var file = fs.readFileSync('weicheng.utf8', "utf8");
 lines = file.split('\n')
 
 var begin = new Date();
+console.log(begin);
 for (var cnt = 0; cnt < 50; cnt++) {
   for (var i = 0; i < lines.length; i++) {
     nodejieba.cut(lines[i]);
   }
 }
 var end = new Date();
+console.log(end);
 var ms = end.getTime() - begin.getTime();
 
 console.log(ms + " ms");
