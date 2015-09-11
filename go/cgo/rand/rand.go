cdd63a73ba6d9e0faf97134d63e0c39c1c7935f7
yanyiwu <wuyanyi09@foxmail.com>
2015-09-11 22:39:43 +0800

    rand

---
+++ go/cgo/rand/rand.go
@@ -0,0 +1,19 @@
+package main
+
+/*
+#include <stdlib.h>
+*/
+import "C"
+import "fmt"
+
+func Random() int {
+	return int(C.random())
+}
+
+func Seed(i int) {
+	C.srandom(C.uint(i))
+}
+
+func main() {
+	fmt.Println(Random())
+}
