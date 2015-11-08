b85fcd12d3954f46530dd21703c1e31cdcffe0b0
yanyiwu <wuyanyi09@foxmail.com>
2015-11-08 15:58:03 +0800

    float

---
+++ cpp/float/divide.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <string>
+#include <exception>
+
+using namespace std;
+
+int main() {
+  float x = 1.0f;
+  float y = 0.0f;
+  float z = x / y;
+  float w = z*10;
+  cout << z << endl;
+  cout << w << endl;
+  cout << 1/float(0) << endl;
+  cout << 1.0/0 << endl;
+  try {
+    cout << 1/0 << endl; // crash
+  } catch(std::exception& e) {
+    cout << e.what() << endl;
+  } catch(...) {
+    cout << "something wrong" << endl;
+  }
+  return 0;
+}
+
