95a26a7b7f467dc19e57cd68c09f0ef4c8eabb4d
yanyiwu <wuyanyi09@foxmail.com>
2015-12-05 18:21:24 +0800

    rust

--- rust/test1/src/main.rs
+++ rust/test1/src/main.rs
@@ -1,3 +1,9 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+    println!("input your guess");
+    let mut guess = String::new();
+    io::stdin().read_line(&mut guess).ok().expect("Failed to read line");
+    println!("your guess:{}", guess);
 }
