5e31ec4cb7bfa84a83c6d35e0a64008be739b688
yanyiwu <i@yanyiwu.com>
2015-12-13 20:21:38 +0800

    coroutine

--- cpp/coroutine/test3.cc
+++ cpp/coroutine/test3.cc
@@ -3,6 +3,11 @@
 
 using namespace std;
 
+typedef void (*Function)(void* arg);
+
+const size_t STACK_SIZE = 1024*1024;
+char stack[STACK_SIZE];
+
 ucontext_t ucp;
 ucontext_t ucp2;
 
@@ -18,10 +23,18 @@ void Tom() {
   cout << "Tom End" << endl;
 }
 
+void Jim() {
+  cout << "Jim Begin" << endl;
+  cout << "Jim End" << endl;
+}
+
 int main() {
   Foo();
+  ucp.uc_stack.ss_sp = stack;
+  ucp.uc_stack.ss_size = sizeof(stack)/sizeof(*stack);
+  ucp.uc_link = &ucp2;
+  makecontext(&ucp, &Jim, 0);
   Tom();
-  setcontext(&ucp2);
   cout << "main exit" << endl;
   return 0;
 }
