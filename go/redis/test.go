30ef356e8f5c9f865ff3ec1096f21ce378ad7d1d
yanyiwu <i@yanyiwu.com>
2015-12-20 22:55:36 +0800

    go redis

---
+++ go/redis/test.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+
+	"gopkg.in/redis.v3"
+)
+
+func ExampleClient() {
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	pong, err := client.Ping().Result()
+	fmt.Println(pong, err)
+	// Output: PONG <nil>
+	err = client.Set("key", "value", 0).Err()
+	if err != nil {
+		panic(err)
+	}
+
+	val, err := client.Get("key").Result()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("key", val)
+
+	val2, err := client.Get("key2").Result()
+	if err == redis.Nil {
+		fmt.Println("key2 does not exists")
+	} else if err != nil {
+		panic(err)
+	} else {
+		fmt.Println("key2", val2)
+	}
+	// Output: key value
+	// key2 does not exists
+}
+
+func main() {
+	ExampleClient()
+}
