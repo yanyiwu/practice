44f6ab0530d7a96e33d68153a666732ca9181289
yanyiwu <i@yanyiwu.com>
2015-12-26 23:32:16 +0800

    ci

--- nodejs/http/test2/test.js
+++ nodejs/http/test2/test.js
@@ -1,37 +1,43 @@
 var http = require('http');
 
-var words = ["x1", "x2", "x3", "x4"];
+function search(word, callback) {
+  var options = {
+    host: 'www.baidu.com',
+    path: '/?wd=x'
+  };
+  http.request(options, function(response) {
+    var str = '';
 
-function search(words, callback) {
+    //another chunk of data has been recieved, so append it to `str`
+    response.on('data', function (chunk) {
+      str += chunk;
+    });
+
+    //the whole response has been recieved, so we just print it out here
+    response.on('end', function () {
+      //console.log(count);
+      //console.log(str.length);
+      callback(str);
+    });
+  }).end();
+}
+
+function multiSearch(words, callback) {
   var count = 0;
   var searchResults = [];
   for (var i = 0; i < words.length; i++) {
-    var options = {
-      host: 'www.baidu.com',
-      path: '/?wd=x'
-    };
-    http.request(options, function(response) {
-      var str = '';
-
-      //another chunk of data has been recieved, so append it to `str`
-      response.on('data', function (chunk) {
-        str += chunk;
-      });
-
-      //the whole response has been recieved, so we just print it out here
-      response.on('end', function () {
-        searchResults[count++] = str;
-        //console.log(count);
-        //console.log(str.length);
-        if (count == words.length) {
-            callback(searchResults);
-        }
-      });
-    }).end();
+    search(words[i], function (res) {
+      searchResults[count++] = res;
+      if (count == words.length) {
+        callback(searchResults);
+      } 
+    })
   }
 }
 
-search(words, function(responses) {
+var words = ["x1", "x2", "x3", "x4"];
+
+multiSearch(words, function(responses) {
   var sum = 0;
   for (var i = 0; i < responses.length; i++) {
     sum += responses[i].length;
