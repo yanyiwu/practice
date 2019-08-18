70b72ee2b655a7039e844493ad8f344d08d106b8
yanyiwu <i@yanyiwu.com>
2019-08-19 00:03:23 +0800

    t

--- ml/fashion_mnist/util.py
+++ ml/fashion_mnist/util.py
@@ -15,7 +15,7 @@ def decode_idx3_ubyte(idx3_ubyte_file):
     offset = 0
     fmt_header = '>iiii'
     magic_number, num_images, num_rows, num_cols = struct.unpack_from(fmt_header, bin_data, offset)
-    print '魔数:%d, 图片数量: %d张, 图片大小: %d*%d' % (magic_number, num_images, num_rows, num_cols)
+    #print '魔数:%d, 图片数量: %d张, 图片大小: %d*%d' % (magic_number, num_images, num_rows, num_cols)
 
     # 解析数据集
     image_size = num_rows * num_cols
@@ -23,8 +23,8 @@ def decode_idx3_ubyte(idx3_ubyte_file):
     fmt_image = '>' + str(image_size) + 'B'
     images = np.empty((num_images, num_rows, num_cols))
     for i in range(num_images):
-        if (i + 1) % 10000 == 0:
-            print '已解析 %d' % (i + 1) + '张'
+        #if (i + 1) % 10000 == 0:
+        #    print '已解析 %d' % (i + 1) + '张'
         images[i] = np.array(struct.unpack_from(fmt_image, bin_data, offset)).reshape((num_rows, num_cols))
         offset += struct.calcsize(fmt_image)
     return images
@@ -42,40 +42,35 @@ def decode_idx1_ubyte(idx1_ubyte_file):
     offset = 0
     fmt_header = '>ii'
     magic_number, num_images = struct.unpack_from(fmt_header, bin_data, offset)
-    print '魔数:%d, 图片数量: %d张' % (magic_number, num_images)
+    #print '魔数:%d, 图片数量: %d张' % (magic_number, num_images)
 
     # 解析数据集
     offset += struct.calcsize(fmt_header)
     fmt_image = '>B'
     labels = np.empty(num_images)
     for i in range(num_images):
-        if (i + 1) % 10000 == 0:
-            print '已解析 %d' % (i + 1) + '张'
+        #if (i + 1) % 10000 == 0:
+        #    print '已解析 %d' % (i + 1) + '张'
         labels[i] = struct.unpack_from(fmt_image, bin_data, offset)[0]
         offset += struct.calcsize(fmt_image)
     return labels
 
-# [(100,28,28)...]
-def load_images_data():
+def load_train_data_batch(batch_size=100):
     images = decode_idx3_ubyte('./data/t10k-images-idx3-ubyte')
-    return images
-
-def load_labels_data():
     labels = decode_idx1_ubyte('./data/t10k-labels-idx1-ubyte')
-    return labels
-
-def load_images_data_batch(batch_size=100):
-    images = load_images_data()
-    batch_size = 100
     for i in range(0, len(images), batch_size):
         imgs = images[i:i+batch_size]
-        yield imgs
+        lbs = labels[i:i+batch_size]
+        yield imgs, lbs
+
+
+def softmax(x):
+    """Compute softmax values for each sets of scores in x."""
+    #print 'xxx', np.sum(np.exp(x))
+    #print 'xxx', np.exp(x)
+    #print 'xxx axis=0', np.sum(np.exp(x), axis=0)
+    #print 'xxx axis=1', np.sum(np.exp(x), axis=1)
+    return np.exp(x-np.max(x, axis=0)) / np.sum(np.exp(x-np.max(x, axis=0)), axis=0)
 
 if __name__ == '__main__':
-    images = load_images_data()
-    batch_size = 100
-    for i in range(0, len(images), batch_size):
-        imgs = images[i:i+batch_size]
-        print imgs.shape
-        #print img.shape
-    #print decode_idx1_ubyte('./data/t10k-labels-idx1-ubyte')
+    pass
