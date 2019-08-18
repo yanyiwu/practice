da8bdee0bce1a9f718c9167a8e0848f4940a5c0f
yanyiwu <i@yanyiwu.com>
2019-08-18 14:04:41 +0800

    util

---
+++ ml/fashion_mnist/util.py
@@ -0,0 +1,63 @@
+#coding: utf-8
+import numpy as np
+import struct
+
+def decode_idx3_ubyte(idx3_ubyte_file):
+    """
+    解析idx3文件的通用函数
+    :param idx3_ubyte_file: idx3文件路径
+    :return: 数据集
+    """
+    # 读取二进制数据
+    bin_data = open(idx3_ubyte_file, 'rb').read()
+
+    # 解析文件头信息，依次为魔数、图片数量、每张图片高、每张图片宽
+    offset = 0
+    fmt_header = '>iiii'
+    magic_number, num_images, num_rows, num_cols = struct.unpack_from(fmt_header, bin_data, offset)
+    print '魔数:%d, 图片数量: %d张, 图片大小: %d*%d' % (magic_number, num_images, num_rows, num_cols)
+
+    # 解析数据集
+    image_size = num_rows * num_cols
+    offset += struct.calcsize(fmt_header)
+    fmt_image = '>' + str(image_size) + 'B'
+    images = np.empty((num_images, num_rows, num_cols))
+    for i in range(num_images):
+        if (i + 1) % 10000 == 0:
+            print '已解析 %d' % (i + 1) + '张'
+        images[i] = np.array(struct.unpack_from(fmt_image, bin_data, offset)).reshape((num_rows, num_cols))
+        offset += struct.calcsize(fmt_image)
+    return images
+
+def decode_idx1_ubyte(idx1_ubyte_file):
+    """
+    解析idx1文件的通用函数
+    :param idx1_ubyte_file: idx1文件路径
+    :return: 数据集
+    """
+    # 读取二进制数据
+    bin_data = open(idx1_ubyte_file, 'rb').read()
+
+    # 解析文件头信息，依次为魔数和标签数
+    offset = 0
+    fmt_header = '>ii'
+    magic_number, num_images = struct.unpack_from(fmt_header, bin_data, offset)
+    print '魔数:%d, 图片数量: %d张' % (magic_number, num_images)
+
+    # 解析数据集
+    offset += struct.calcsize(fmt_header)
+    fmt_image = '>B'
+    labels = np.empty(num_images)
+    for i in range(num_images):
+        if (i + 1) % 10000 == 0:
+            print '已解析 %d' % (i + 1) + '张'
+        labels[i] = struct.unpack_from(fmt_image, bin_data, offset)[0]
+        offset += struct.calcsize(fmt_image)
+    return labels
+
+if __name__ == '__main__':
+    images = decode_idx3_ubyte('./data/t10k-images-idx3-ubyte')
+    for img in images:
+        print img
+        print img.shape
+    #print decode_idx1_ubyte('./data/t10k-labels-idx1-ubyte')
