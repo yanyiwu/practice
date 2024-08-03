1b637cee7b3bde5e06aa52f7f6ef7c9acb033c71
yanyiwu <wuyanyi09@foxmail.com>
2024-08-03 12:25:19 +0800

    [criteo_ctr]

---
+++ criteo_ctr/test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from datasets import load_dataset
+import tensorflow as tf
+
+ds = load_dataset("reczoo/Criteo_x1")
+
+print(ds)
+print(ds.keys())
+
+
+print(ds)
+print(ds.keys())
