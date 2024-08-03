c10302bd5c0c5bf3adfce680a2f0d4b442c849df
yanyiwu <wuyanyi09@foxmail.com>
2024-08-03 19:26:50 +0800

    [criteo ctr] tidy

--- criteo_ctr/test.py
+++ criteo_ctr/test.py
@@ -11,16 +11,17 @@ ds = ds['train'].train_test_split(test_size=0.05)
 print(ds)
 
 FEATURE_COLUMNS = [f'I{i}' for i in range(1, 14)] + [f'C{i}' for i in range(1, 27)]
+print(FEATURE_COLUMNS)
 LABEL_COLUMN = 'label'
 
-def preprocess(features):
-    for col in [f'C{i}' for i in range(1, 27)]:
-        features[col] = tf.strings.to_hash_bucket_fast(tf.strings.as_string(features[col]), 1000)
-    for col in [f'I{i}' for i in range(1, 14)]:
-        features[col] = tf.cast(features[col], tf.float32)
-    return features
-
 def to_tf_dataset(dataset):
+    def preprocess(features):
+        for col in [f'C{i}' for i in range(1, 27)]:
+            features[col] = tf.strings.to_hash_bucket_fast(tf.strings.as_string(features[col]), 1000)
+        for col in [f'I{i}' for i in range(1, 14)]:
+            features[col] = tf.cast(features[col], tf.float32)
+        return features
+
     def gen():
         for row in dataset:
             features = {key: row[key] for key in FEATURE_COLUMNS}
@@ -47,8 +48,8 @@ numerical_inputs = [layers.Reshape((1,))(inputs[col]) for col in [f'I{i}' for i 
 
 # 合并所有特征
 x = layers.Concatenate()(numerical_inputs + embeddings)
-x = layers.Dense(256, activation='relu')(x)
-x = layers.Dense(128, activation='relu')(x)
+#x = layers.Dense(256, activation='relu')(x)
+#x = layers.Dense(128, activation='relu')(x)
 output = layers.Dense(1, activation='sigmoid')(x)
 
 model = Model(inputs=inputs, outputs=output)
