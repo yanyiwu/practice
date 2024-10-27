90d2d66775aa8edc6f0755136ce790bc3325a7da
yanyiwu <wuyanyi09@foxmail.com>
2024-10-27 20:53:49 +0800

    cursor

--- transformer.py
+++ transformer.py
@@ -140,15 +140,26 @@ class DecoderLayer(tf.keras.layers.Layer):
         self.dropout3 = tf.keras.layers.Dropout(rate)
 
     def call(self, x, enc_output, training, look_ahead_mask, padding_mask):
+        # x shape: (batch_size, target_seq_len, d_model)    
+        # enc_output shape: (batch_size, input_seq_len, d_model)    
         attn1, attn_weights_block1 = self.mha1(x, x, x, look_ahead_mask)
+        # attn1 shape: (batch_size, target_seq_len, d_model)
         attn1 = self.dropout1(attn1, training=training)
+        # attn1 shape: (batch_size, target_seq_len, d_model)
         out1 = self.layernorm1(attn1 + x)
+        # out1 shape: (batch_size, target_seq_len, d_model)
         attn2, attn_weights_block2 = self.mha2(enc_output, enc_output, out1, padding_mask)
+        # attn2 shape: (batch_size, target_seq_len, d_model)
         attn2 = self.dropout2(attn2, training=training)
+        # attn2 shape: (batch_size, target_seq_len, d_model)
         out2 = self.layernorm2(attn2 + out1)
+        # out2 shape: (batch_size, target_seq_len, d_model)
         ffn_output = self.ffn(out2)
+        # ffn_output shape: (batch_size, target_seq_len, d_model)   
         ffn_output = self.dropout3(ffn_output, training=training)
+        # ffn_output shape: (batch_size, target_seq_len, d_model)
         out3 = self.layernorm3(ffn_output + out2)
+        # out3 shape: (batch_size, target_seq_len, d_model)
         return out3, attn_weights_block1, attn_weights_block2
 
 class Encoder(tf.keras.layers.Layer):
@@ -189,16 +200,25 @@ class Decoder(tf.keras.layers.Layer):
         self.dropout = tf.keras.layers.Dropout(rate)
 
     def call(self, x, enc_output, training=None, look_ahead_mask=None, padding_mask=None):
+        # enc_output shape: (batch_size, input_seq_len, d_model)
+        # x shape: (batch_size, target_seq_len) 
         seq_len = tf.shape(x)[1]
+        # seq_len shape: target_seq_len
         attention_weights = {}
         x = self.embedding(x)
+        # x shape: (batch_size, target_seq_len, d_model)
         x *= tf.math.sqrt(tf.cast(self.d_model, tf.float32))
+        # x shape: (batch_size, target_seq_len, d_model)
         x += self.pos_encoding[:, :seq_len, :]
+        # x shape: (batch_size, target_seq_len, d_model)
         x = self.dropout(x, training=training)
         for i in range(self.num_layers):
             x, block1, block2 = self.dec_layers[i](x, enc_output, training=training, 
                                                    look_ahead_mask=look_ahead_mask, 
                                                    padding_mask=padding_mask)
+            # x shape: (batch_size, target_seq_len, d_model)    
+            # block1 shape: (batch_size, num_heads, target_seq_len, input_seq_len)
+            # block2 shape: (batch_size, num_heads, target_seq_len, input_seq_len)  
             attention_weights[f'decoder_layer{i+1}_block1'] = block1
             attention_weights[f'decoder_layer{i+1}_block2'] = block2
         return x, attention_weights
